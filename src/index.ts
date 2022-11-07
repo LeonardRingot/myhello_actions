9f7c8b904664b93031eca1d94b1537dce994feb4
LeonardRingot <leonardringot6@gmail.com>
2022-11-07 13:19:14 +0100

    firstcommitleo

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
 
-app.listen(3000, ()=> console.log("Application démarrée sur le port 3000"));
+app.listen(3005, ()=> console.log("je suis leonard ringot sur le port 3005"));
