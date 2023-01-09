3fd1c87d27efe5ee03d6d0bf8f623571b0407d0a
LeonardRingot <leonardringot6@gmail.com>
2023-01-09 18:05:25 +0100

    test dockerd

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
 
-app.listen(3005, ()=> console.log("je suis leonard ringot sur le port 3005"));
+app.listen(3005, ()=> console.log("je suis leonard ringot sur le port 3005 et c partiiiiiii"));
