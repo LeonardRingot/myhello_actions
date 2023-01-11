df43b66015b06ca3dbc33616de9872bd187ea771
LeonardRingot <leonardringot6@gmail.com>
2023-01-11 11:43:56 +0100

    et maintenant ??

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Simplon'));
 
-app.listen(3005, ()=> console.log("je suis leonard ringot sur le port 3005 et zéé partiiiiiii  "));
+app.listen(3005, ()=> console.log("je suis leonard ringot sur le port 3005 OUIIIIIIIIII  "));
