ef7c20c73a60c82120034209b490dc0280fb45c5
LeonardRingot <leonardringot6@gmail.com>
2023-01-12 15:09:00 +0100

    fezef

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express()
-
+app.get('/docker',(req,res)=>res.send('ca fonctionne oui ou merde'));
 app.get('/hello', (req, res) => res.send('Hello Simplonyuyu'));
 
 app.listen(3005, ()=> console.log("je suis leonard ringot sur le port 3005 OUIIIIIIIIII  "));
