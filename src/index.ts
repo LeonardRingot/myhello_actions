39d92d4966dc19128dfe29026c970cf289177c26
LeonardRingot <leonardringot6@gmail.com>
2023-01-13 11:40:49 +0100

    z

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express()
 app.get('/docker',(req,res)=>res.send(process.env.ID));
 app.get('/hello', (req, res) => res.send('Hello Simplona'));
