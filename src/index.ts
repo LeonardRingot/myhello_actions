c0e7207be227af7c2d82c90cc1c92c6a42aed5fe
LeonardRingot <leonardringot6@gmail.com>
2023-01-13 11:21:37 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 app.get('/docker',(req,res)=>res.send('DOCKER'));
-app.get('/hello', (req, res) => res.send('Hello Simplon'));
+app.get('/hello', (req, res) => res.send('Hello Simplona'));
 
 app.listen(3005, ()=> console.log("je suis leonard ringot sur le port 3005"));
